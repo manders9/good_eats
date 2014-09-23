8ea06c7d9872b41afc8367d97ce24edc51d9d24a
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-09-22 20:49:01 -0400

    Nested routes created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :restaurants do
+    resources :reviews
+  end
+
+  match '/' => 'restaurants#root', via: :get, as: :root
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
